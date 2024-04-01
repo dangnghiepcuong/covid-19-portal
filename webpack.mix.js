80990a0df45418248014c2e3f51331969ba96cdd
cuongdn-3440 <dang.nghiep.cuong@sun-asterisk.com>
2024-04-01 11:23:11 +0700

    remove unused asset

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,7 +17,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/getLocalRegion.js', 'public/js')
-    .js('resources/js/test.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss'),
         require('autoprefixer'),
