0f2e912dd78eaae18c6f5b5bede56e8fa0a5b2d7
cuongdn-3440 <dang.nghiep.cuong@sun-asterisk.com>
2024-04-11 17:00:53 +0700

    fix UI display business resource

--- resources/views/business/index.blade.php
+++ resources/views/business/index.blade.php
@@ -4,76 +4,90 @@
             {{ __('object.list', ['object' => __('business.business')]) }}
         </h2>
     </x-slot>
-    <a class="btn btn-primary my-3" href="{{ route('businesses.create') }}">{{ __('btn.create', ['object' => __('account.account')]) }}</a>
-    <a class="btn btn-secondary my-3" href="{{ route('businesses.trashed') }}">{{ __('object.deactivated', ['object' => __('account.account')]) }}</a>
 
-    <table class="table table-hover">
-        <tr>
-            <th class="text-center">
-                #
-            </th>
-            <th class="text-center">
-                {{ __('account.account') }}
-            </th>
-            <th class="text-center">
-                {{ __('business.tax_id') }}
-            </th>
-            <th class="text-center">
-                {{ __('business.name') }}
-            </th>
-            <th class="text-center">
-                {{ __('business.province') }}
-            </th>
-            <th class="text-center">
-                {{ __('business.district') }}
-            </th>
-            <th class="text-center">
-                {{ __('business.ward') }}
-            </th>
-            <th class="text-center">
-                {{ __('btn.action') }}
-            </th>
-        </tr>
-        @if ($businesses !== null)
-            $i = 0;
-            @foreach ($businesses as $business)
-            <tr>
-                <td class="text-center">
-                    {{ ++$i; }}
-                </td>
-                <th class="text-center">
-                    {{ $business->account->email }}
-                </th>
-                <td class="text-center">
-                    {{ $business->tax_id }}
-                </td>
-                <td class="text-center">
-                    {{ $business->name }}
-                </td>
-                <td class="text-center">
-                    {{ $business->addr_province }}
-                </td>
-                <td class="text-center">
-                    {{ $business->addr_district }}
-                </td>
-                <td class="text-center">
-                    {{ $business->addr_ward }}
-                </td>
-                <td class="flex justify-center">
-                    <a href="{{ route('businesses.show', $business->id) }}">{{ __('btn.view') }}</a>
-                    <p>&nbsp/&nbsp</p>
-                    <a href="{{ route('businesses.edit', $business->id) }}">{{ __('btn.edit', ['object' => '']) }}</a>
-                    <p>&nbsp/&nbsp</p>
-                    <form method="POST" action="{{ route('businesses.destroy', $business->id) }}">
-                        @csrf
-                        @method('delete')
-                        <input type="submit" value="{{ __('btn.delete') }}" onclick="confirm('Are you sure you want to delete?')">
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-        @endif
-    </table>
+    <div class="my-3">
+        <a class="btn btn-primary text-truncate my-1"
+            href="{{ route('businesses.create') }}">{{ __('btn.create', ['object' => __('account.account')]) }}</a>
+        <a class="btn btn-secondary text-truncate my-1"
+            href="{{ route('businesses.trashed') }}">{{ __('object.deactivated', ['object' => __('account.account')]) }}</a>
+    </div>
+
+    <div class="h-600px overflow-x-scroll">
+        <table class="table table-responsive table-hover w-full">
+            <thead>
+                <tr>
+                    <th class="text-center text-truncate">
+                        #
+                    </th>
+                    <th class="text-center text-truncate">
+                        {{ __('account.account') }}
+                    </th>
+                    <th class="text-center text-truncate">
+                        {{ __('business.tax_id') }}
+                    </th>
+                    <th class="text-center text-truncate">
+                        {{ __('business.name') }}
+                    </th>
+                    <th class="text-center text-truncate">
+                        {{ __('business.province') }}
+                    </th>
+                    <th class="text-center text-truncate">
+                        {{ __('business.district') }}
+                    </th>
+                    <th class="text-center text-truncate">
+                        {{ __('business.ward') }}
+                    </th>
+                    <th class="text-center text-truncate">
+                        {{ __('btn.action') }}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @if ($businesses !== null)
+                    @php
+                        $i = 0;
+                    @endphp
+                    @foreach ($businesses as $business)
+                        <tr>
+                            <td class="text-left text-truncate">
+                                {{ ++$i }}
+                            </td>
+                            <th class="text-left text-truncate">
+                                {{ $business->account->email }}
+                            </th>
+                            <td class="text-left text-truncate">
+                                {{ $business->tax_id }}
+                            </td>
+                            <td class="text-left text-truncate">
+                                {{ $business->name }}
+                            </td>
+                            <td class="text-left text-truncate">
+                                {{ $business->addr_province }}
+                            </td>
+                            <td class="text-left text-truncate">
+                                {{ $business->addr_district }}
+                            </td>
+                            <td class="text-left text-truncate">
+                                {{ $business->addr_ward }}
+                            </td>
+                            <td class="flex justify-center flex-gap-3px">
+                                <a class="btn btn-primary"
+                                    href="{{ route('businesses.show', $business->id) }}">{{ __('btn.view') }}</a>
+                                <a class="btn btn-secondary"
+                                    href="{{ route('businesses.edit', $business->id) }}">{{ __('btn.edit', ['object' => '']) }}</a>
+                                <form method="POST" action="{{ route('businesses.destroy', $business->id) }}">
+                                    @csrf
+                                    @method('delete')
+                                    <button class="btn btn-danger color-danger" type="submit"
+                                        onclick="confirm('Are you sure you want to delete?')">{{ __('btn.delete') }}</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
+            </tbody>
+        </table>
+    </div>
     @if (Session::get('success'))
         <div class="alert alert-success">
             <ul>
--- resources/views/business/trashed.blade.php
+++ resources/views/business/trashed.blade.php
@@ -5,75 +5,83 @@
         </h2>
     </x-slot>
 
-    <table class="table table-hover">
-        <tr>
-            <th class="text-center">
-                #
-            </th>
-            <th class="text-center">
-                {{ __('account.account') }}
-            </th>
-            <th class="text-center">
-                {{ __('business.tax_id') }}
-            </th>
-            <th class="text-center">
-                {{ __('business.name') }}
-            </th>
-            <th class="text-center">
-                {{ __('business.province') }}
-            </th>
-            <th class="text-center">
-                {{ __('business.district') }}
-            </th>
-            <th class="text-center">
-                {{ __('business.ward') }}
-            </th>
-            <th class="text-center">
-                {{ __('btn.action') }}
-            </th>
-        </tr>
-        @php
-        $i = 0;
-        @endphp
-        @if ($businesses !== null)
-        @foreach ($businesses as $business)
-        <tr>
-            <td class="text-center">
-                {{ ++$i; }}
-            </td>
-            <th class="text-center">
-                {{ $business->account->email }}
-            </th>
-            <td class="text-center">
-                {{ $business->tax_id }}
-            </td>
-            <td class="text-center">
-                {{ $business->name }}
-            </td>
-            <td class="text-center">
-                {{ $business->addr_province }}
-            </td>
-            <td class="text-center">
-                {{ $business->addr_district }}
-            </td>
-            <td class="text-center">
-                {{ $business->addr_ward }}
-            </td>
-            <td class="flex justify-center">
-                <form method="POST" action="{{ route('businesses.restore', $business->id) }}">
-                    @csrf
-                    <input type="submit" value="{{ __('btn.restore') }}" onclick="confirm('Are you sure you want to restore?')">
-                </form>
-            </td>
-        </tr>
-        @endforeach
-        @endif
-    </table>
-    @if (Session::get('success'))
-    <div class="alert alert-success">
-        <ul>
-            <li>{{ __('message.success', ['action' => __('btn.restore')]) }}</li>
-        </ul>
+    <div class="h-600px mt-4 overflow-x-scroll">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th class="text-center">
+                        #
+                    </th>
+                    <th class="text-center">
+                        {{ __('account.account') }}
+                    </th>
+                    <th class="text-center">
+                        {{ __('business.tax_id') }}
+                    </th>
+                    <th class="text-center">
+                        {{ __('business.name') }}
+                    </th>
+                    <th class="text-center">
+                        {{ __('business.province') }}
+                    </th>
+                    <th class="text-center">
+                        {{ __('business.district') }}
+                    </th>
+                    <th class="text-center">
+                        {{ __('business.ward') }}
+                    </th>
+                    <th class="text-center">
+                        {{ __('btn.action') }}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @if ($businesses !== null)
+                    @php
+                        $i = 0;
+                    @endphp
+                    @foreach ($businesses as $business)
+                        <tr>
+                            <td class="text-center">
+                                {{ ++$i }}
+                            </td>
+                            <th class="text-center">
+                                {{ $business->account->email }}
+                            </th>
+                            <td class="text-center">
+                                {{ $business->tax_id }}
+                            </td>
+                            <td class="text-center">
+                                {{ $business->name }}
+                            </td>
+                            <td class="text-center">
+                                {{ $business->addr_province }}
+                            </td>
+                            <td class="text-center">
+                                {{ $business->addr_district }}
+                            </td>
+                            <td class="text-center">
+                                {{ $business->addr_ward }}
+                            </td>
+                            <td class="flex justify-center">
+                                <form method="POST" action="{{ route('businesses.restore', $business->id) }}">
+                                    @csrf
+                                    <input type="submit" class="btn btn-success color-success"
+                                        value="{{ __('btn.restore') }}"
+                                        onclick="confirm('Are you sure you want to restore?')">
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
+            </tbody>
+        </table>
     </div>
+    @if (Session::get('success'))
+        <div class="alert alert-success">
+            <ul>
+                <li>{{ __('message.success', ['action' => __('btn.restore')]) }}</li>
+            </ul>
+        </div>
     @endif
 </x-app-layout>
